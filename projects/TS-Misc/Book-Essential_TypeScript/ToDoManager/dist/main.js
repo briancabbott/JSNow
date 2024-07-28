db412a4ccf35436fcb3401564c3e035fb153cd7e
Brian Abbott <briancabbott@gmail.com>
2024-07-28 13:19:25 -0700

    adding changes

--- projects/TS-Misc/Book-Essential_TypeScript/ToDoManager/dist/main.js
+++ projects/TS-Misc/Book-Essential_TypeScript/ToDoManager/dist/main.js
@@ -1,5 +1,6 @@
 import { ToDoItem } from "./todoItem.js";
 import { ToDoCollection } from "./todoCollection.js";
+import inquirer from "inquirer";
 let todos = [
     new ToDoItem(1, "qwerty"),
     new ToDoItem(2, "asdf"),
@@ -7,8 +8,26 @@ let todos = [
     new ToDoItem(4, "yuiop", true)
 ];
 let collection = new ToDoCollection("Brian", todos);
-console.clear();
-console.log(`${collection.userName}'s ToDo List`);
-let newId = collection.addToDo("Read Graph Theory Stuff");
-let todoItem = collection.getToDoById(newId);
-todoItem.printDetails();
+function displayToDoList() {
+    console.log(`${collection.userName}'s ToDo List (${collection.getItemCounts().incomplete} items to do)`);
+    collection.getToDoItems(true).forEach(item => item.printDetails());
+}
+var Commands;
+(function (Commands) {
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
+function promptUser() {
+    console.clear();
+    displayToDoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose Option",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers["command"] !== Commands.Quit) {
+            promptUser();
+        }
+    });
+}
+promptUser();
--- projects/TS-Misc/Book-Essential_TypeScript/ToDoManager/src/main.ts
+++ projects/TS-Misc/Book-Essential_TypeScript/ToDoManager/src/main.ts
@@ -1,6 +1,7 @@
 
 import { ToDoItem } from "./todoItem.js";
 import { ToDoCollection } from "./todoCollection.js";
+import inquirer from "inquirer";
 
 let todos: ToDoItem[] = [
     new ToDoItem(1, "qwerty"),
@@ -11,9 +12,27 @@ let todos: ToDoItem[] = [
 
 let collection: ToDoCollection = new ToDoCollection("Brian", todos);
 
-console.clear();
-console.log(`${collection.userName}'s ToDo List`);
+function displayToDoList(): void {
+    console.log(`${collection.userName}'s ToDo List (${collection.getItemCounts().incomplete} items to do)`);
+    collection.getToDoItems(true).forEach(item => item.printDetails());
+}
 
-let newId: number = collection.addToDo("Read Graph Theory Stuff");
-let todoItem: ToDoItem = collection.getToDoById(newId);
-todoItem.printDetails();
+enum Commands {
+    Quit = "Quit"
+}
+
+function promptUser(): void {
+    console.clear();
+    displayToDoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose Option",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers["command"] !== Commands.Quit) {
+            promptUser();
+        }
+    })
+}
+promptUser();
